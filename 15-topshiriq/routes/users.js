7a108e5729fc1b22e1b79d98f473967718d1b33c
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-07-16 14:53:24 +0500

    auth

---
+++ 15-topshiriq/routes/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt");
+const { User, validate } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const _ = require("lodash");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).send("Mavjud bolgan foydalanuvchi");
+  }
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  const salt = await bcrypt.genSalt();
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+  //   const { name, email } = user;
+  res.send(_.pick(user, ["_id", "name", "email"]));
+});
+
+module.exports = router;
