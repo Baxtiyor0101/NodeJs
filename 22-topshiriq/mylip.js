084ee1dafca1e4431979cacb22fec0aa23fbfcec
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-07-21 15:09:25 +0500

    matnni testlash

---
+++ 22-topshiriq/mylip.js
@@ -0,0 +1,9 @@
+// sonlarni test qilish -- mutlaq qiymatini topish kk
+module.exports.absalute = function (number) {
+  return number >= 0 ? number : -number;
+};
+
+// Matnlrni test qilish
+module.exports.salom = function (name) {
+  return "Assalomu alaykum hammaga, " + name;
+};
--- 22-topshiriq/tests/mylib.test.js
+++ 22-topshiriq/tests/mylib.test.js
@@ -1,3 +1,25 @@
-test('mening ilk testim', () => { 
-    throw new Error('nimadir ishlamay  qoldiku shuning uchun xatolikga uchradi')
- })
+const mylip = require("../mylip");
+describe("absolute", () => {
+  it(" should return a positive number ifinput is positive", () => {
+    const result = mylip.absalute(1);
+    expect(result).toBe(1);
+  });
+
+  it(" should return a positive number ifinput is negative", () => {
+    const result = mylip.absalute(-1);
+    expect(result).toBe(1);
+  });
+
+  it(" should return zero ifinput is positive", () => {
+    const result = mylip.absalute(0);
+    expect(result).toBe(0);
+  });
+});
+
+describe("salom", () => {
+  it("should return greeting message", () => {
+    const result = mylip.salom("Baxtiyor");
+    // expect(result).toContain("Baxtiyor");
+    expect(result).toMatch(/Baxtiyor/);
+  });
+});
