bb400c336ee5b492607ec57f98d616b218eee207
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-07-24 12:18:41 +0500

    testinglast

---
+++ 23-topshiriq/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const winston = require("winston");
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  winston.info(`${port}chi portni eshitishni boshladim...`);
+});
+
+module.exports = server;
